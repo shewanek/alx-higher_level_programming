1b9f44805be2e8c651d68751443244d186093d12
shewanek <shewa@kali.cbo>
2022-11-30 22:23:11 -0500

    Python - Hello, World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,12 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <lists.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
