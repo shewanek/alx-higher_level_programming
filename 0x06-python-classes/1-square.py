ba1d22bb731e742c0131a29a9bb41dc23fce43d1
Shewanek Zewdu <shz.cbo@gmail.com>
2022-12-20 16:08:00 +0300

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-No module imported
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    Defines a square by private instance size,
-    instantiation with size.
-    """
-    def __init__(self, size):
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
-        size : no type/value verification
-        ----------------------------
-        Private instance attribute size
-        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
